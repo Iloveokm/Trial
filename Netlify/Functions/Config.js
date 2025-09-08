5d58e2073e9fd64db5eb1beb2d1c6f435b3b5bb7
Om Parkash <109645826+Iloveokm@users.noreply.github.com>
2025-09-08 13:59:53 +0530

    Update Config.js

--- Netlify/Functions/Config.js
+++ Netlify/Functions/Config.js
@@ -1,27 +1,22 @@
-// netlify/functions/config.js
-
 exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      firebaseConfig: {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: "dekha-5daeb.firebaseapp.com",
-        databaseURL: "https://dekha-5daeb-default-rtdb.firebaseio.com",
-        projectId: "dekha-5daeb",
-        storageBucket: "dekha-5daeb.firebasestorage.app",
-        messagingSenderId: "647841969799",
-        appId: "1:647841969799:web:c133fdc1cc8ea986846a22"
-      },
-      cloudinaryConfig: {
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET
-      },
-      GIPHY_API_KEY: process.env.GIPHY_API_KEY,
-      ADMIN_EMAIL: process.env.ADMIN_EMAIL
-    }),
-  };
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            firebaseConfig: {
+                apiKey: process.env.FIREBASE_API_KEY,
+                authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+                databaseURL: process.env.FIREBASE_DATABASE_URL,
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+                messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+                appId: process.env.FIREBASE_APP_ID
+            },
+            cloudinaryConfig: {
+                cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+                upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET
+            },
+            ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+            GIPHY_API_KEY: process.env.GIPHY_API_KEY
+        })
+    };
 };
